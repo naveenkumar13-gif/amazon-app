7e4bfaeee7959f048afa91abe17247b125d7c3f1
naveenkumar13-gif <naveenkumarofficial1316@gmail.com>
2024-12-14 16:04:29 +0530

    pradouct commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0LUy-FZq-eaFeQu9WbxFBaM4BQSLFSfU",
+  authDomain: "clone-558c5.firebaseapp.com",
+  projectId: "clone-558c5",
+  storageBucket: "clone-558c5.firebasestorage.app",
+  messagingSenderId: "939646833407",
+  appId: "1:939646833407:web:5d1259879df41a2671f2ca",
+  measurementId: "G-FTMNXSJGXB",
+};
