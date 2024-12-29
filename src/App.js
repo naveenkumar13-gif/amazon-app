a30d6690d50d514e35be3a3830ecbfe08aac974d
naveenkumar13-gif <naveenkumarofficial1316@gmail.com>
2024-12-29 19:13:53 +0530

    add Stripe integration for payment processing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import { auth } from "./Pages/firebase";
 import { useDispatch } from "react-redux";
 import { setUser } from "./components/feature/ActionSlice";
 import Payment from "./Pages/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51QbLkuPwzOckrqyVu96Mf6b9eEkCD5ise3j7BJQVrsozQzlYnwkDrsHdpLsIuYZn2LlvR3CCFRj9OPNO1m7sOUMx004QS3YTgG"
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -35,7 +41,14 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="login" element={<Login />} />
           <Route path="/checkout" element={<Checkout />} />
-          <Route path="/payment" element={<Payment />} />
+          <Route
+            path="/payment"
+            element={
+              <Elements stripe={stripePromise}>
+                <Payment />
+              </Elements>
+            }
+          />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- src/Pages/Payment.js
+++ src/Pages/Payment.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../components/nav/Nav";
 import { useSelector } from "react-redux";
 import CheckOutItem from "../components/checkout/CheckOutItem";
 import { Link } from "react-router-dom";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
 
 function Payment() {
   const username = useSelector((store) => store.customer.username);
   const ProductCart = useSelector((store) => store.action.cart);
-  const style = "flex p-5 mx-5 border-b border-solid  border-slate-400 gap-5";
+  const Price = useSelector((store) =>
+    // ------------ store --> action == actionSlice----cart
+    store?.action?.cart.reduce((amount, item) => amount + item.price, 0)
+  );
+  const stripe = useStripe();
+  const elements = useElements();
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState("");
+  const [succeeded, setSucceeded] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const style = " p-5 mx-5 gap-5";
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setProcessing(true);
+    // const payload = await stripe
+  }
+  function handleChange(e) {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  }
   return (
     <div>
       <Nav />
       {/* container */}
       <div className=" bg-white">
         <div>
-          <h1 className="text-center p-3 font-medium bg-slate-200 border border-solid border-[#dddddd] text-2xl">
+          <h1 className="text-center p-3 font-medium bg-slate-200  text-2xl">
             Checkout (<Link to="/checkout">{ProductCart?.length} items</Link>)
           </h1>
-          <div className={style}>
+          <div className="border-b border-solid border-[#dddddd]  p-5 mx-5 ">
             <h3 className="text-2xl font-medium">Delivery</h3>
             <div>
               <p>{username}</p>
@@ -28,7 +50,7 @@ function Payment() {
         </div>
 
         <div className={style}>
-          <div className="grid ">
+          <div>
             <h3 className="text-2xl font-medium">Review items and delivery</h3>
           </div>
           <div>
@@ -45,11 +67,37 @@ function Payment() {
           </div>
         </div>
 
-        <div className={style}>
+        <div className=" flex flex-col p-5 w-1/2 m-auto">
           <div>
             <h3 className="text-2xl font-medium">Payment Method</h3>
           </div>
-          <div>{/* stripe */}</div>
+          <div>
+            {/* stripe */}
+            <form onSubmit={handleSubmit}>
+              <div>
+                <CardElement onChange={handleChange} className=" p-2" />
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <h3>Order Total: {Price}</h3>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={0}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+              </div>
+              <button
+                disabled={processing || disabled || succeeded}
+                className="bg-[#f0c14b] border-none p-2 rounded-lg w-full mt-2"
+              >
+                <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+              </button>
+            </form>
+          </div>
+          {error && <div>{error}</div>}
         </div>
       </div>
     </div>
